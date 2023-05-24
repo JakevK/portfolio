784f2868c3544b793fec5491a4ae682646543b6c
JakevK <jakevankeulen@gmail.com>
2023-05-24 13:41:35 +1200

    Setting up analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "/styles/globals.css";
 import "/styles/theme.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
